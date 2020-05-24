1d0d48dc114ef8c323ca4a50145173051f11d171
Kusa-crazy <64383362+kusa-crazy@users.noreply.github.com>
2020-05-24 19:41:42 -0400

    Update install.py

--- install.py
+++ install.py
@@ -11,6 +11,7 @@ if Pkg == "s":
         os.system("pkg install sl -y")
         os.system("pip install requests")
         os.system("mv sbs ../../usr/bin")
+        os.system("mv ~/botscan ../usr/etc
     except:
         print("  HOUVE ALGUM ERRO OU O PROCESSO FOI MORTO!")
     else:
